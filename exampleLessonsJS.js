0676294643dff0192aabb89fd6e53c06ef343180
Blazhi <d.blazhik@gmail.com>
2022-09-17 17:33:41 +0300

    Update exampleLessonsJS.js

--- exampleLessonsJS.js
+++ exampleLessonsJS.js
@@ -165,42 +165,213 @@
 
 ////////////////////////////////////////////////////
 
-const courses = ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL'];
+// const courses = ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL'];
 
-function addCourse(name){
-    if (courses.includes(name)) return;
+// function addCourse(name){
+//     if (courses.includes(name)) return;
      
-    courses.push(name)
+//     courses.push(name)
+// }
+
+
+// function removeCourse(name){
+//     const index = courses.indexOf(name)
+
+//     if (index != -1){
+//         courses.splice(index, 1);
+//     } else{
+//         console.log('Courses not found')
+//     }
+// }
+
+
+// function updateCourse(oldName, newName){
+//     const index = courses.indexOf(oldName)
+
+//     if (index != -1){
+//         courses.splice(index, 1, newName);
+//     } else{
+//         console.log('Courses not found')
+//     }
+// }
+// addCourse('Express');
+// console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL', 'Express']
+// addCourse('CSS'); // 'У вас уже есть такой курс'
+
+// removeCourse('React');
+// // console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'Express']
+// removeCourse('Vue'); // 'Курс с таким имененем не найден'
+
+// updateCourse('Express', 'NestJS');
+// console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'NestJS']
+
+/////////////////////////////////////////////////////////////
+
+// function createProduct(partialProduct, callback) {
+//     const product = { id: Date.now(), ...partialProduct };
+//     callback(product);
+//   }
+  
+//   function logProduct(product) {
+//     console.log(product);
+//   }
+  
+//   function logTotalPrice(product) {
+//     console.log(product.price * product.quantity);
+//   }
+  
+//   createProduct({ name: '🍎', price: 30, quantity: 3 }, logProduct);
+//   createProduct({ name: '🍋', price: 20, quantity: 5 }, logTotalPrice);
+
+//////////////////////////////////////////////////
+
+// const createProduct = (partialProduct, callback) => {
+//     const product = { id: Date.now(), ...partialProduct };
+//     callback(product);
+//   }
+  
+//   const logProduct = (product) => {
+//     console.log(product);
+//   }
+  
+//    const logTotalPrice = (product) => {
+//     console.log(product.price * product.quantity);
+//   }
+  
+//   createProduct({ name: '🍎', price: 30, quantity: 3 }, logProduct);
+//   createProduct({ name: '🍋', price: 20, quantity: 5 }, logTotalPrice);
+
+/////////////////////////////////////////
+
+// const TRANSACTION_LIMIT = 1000;
+
+// const account = {
+//   username: 'Jacob',
+//   balance: 400,
+//   withdraw(amount, onSuccess, onError) {
+//     if (amount > TRANSACTION_LIMIT) {
+//       onError(`Amount should not exceed ${TRANSACTION_LIMIT} credits`);
+//     } else if (amount > this.balance) {
+//       onError(`Amount can't exceed account balance of ${this.balance} credits`);
+//     } else {
+//       this.balance -= amount;
+//       onSuccess(`Account balance: ${this.balance}`);
+//     }
+//   },
+//   deposit(amount, onSuccess, onError) {
+//     if (amount > TRANSACTION_LIMIT) {
+//       onError(`Amount should not exceed ${TRANSACTION_LIMIT} credits`);
+//     } else if (amount <= 0) {
+//       onError(`Amount must be more than 0 credits`);
+//     } else {
+//       this.balance += amount;
+//       onSuccess(`Account balance: ${this.balance}`);
+//     }
+//   },
+// };
+
+// const handleSuccess = (message) => {
+//   console.log(`✅ Success! ${message}`);
+// }
+// const  handleError = (message) => {
+//   console.log(`❌ Error! ${message}`);
+// }
+
+// account.withdraw(2000, handleSuccess, handleError);
+// account.withdraw(600, handleSuccess, handleError);
+// account.withdraw(300, handleSuccess, handleError);
+// account.deposit(1700, handleSuccess, handleError);
+// account.deposit(0, handleSuccess, handleError);
+// account.deposit(-600, handleSuccess, handleError);
+// account.deposit(600, handleSuccess, handleError);
+
+//////////////////////////////////////
+
+// const  each = (array, callback) => {
+//     const newArr = [];
+//     for (const el of array) {
+//       newArr.push(callback(el));
+//     }
+//     return newArr;
+//   }
+  
+//   console.log(
+//     each([64, 49, 36, 25, 16],  (value) => value * 2),
+//   );
+//   console.log(
+//     each([64, 49, 36, 25, 16], (value) => value - 10),
+//   );
+//   console.log(
+//     each([64, 49, 36, 25, 16],  (value) => Math.sqrt(value)),
+//   );
+//   console.log(
+//     each([1.5, 2.1, 16.4, 9.7, 11.3], (value) => Math.ceil(value))
+//   );
+//   console.log(
+//     each([1.5, 2.1, 16.4, 9.7, 11.3], (value) => Math.floor(value))
+//   );
+
+/////////////////////////////////////
+
+// function logItems(items) {
+//     console.log(items);
+    
+//    items.forEach((item,i)=>console.log(`${i + 1} - ${items[i]}`))
+//   }
+  
+//   logItems(['Mango', 'Poly', 'Ajax']);
+//   logItems(['🍎', '🍇', '🍑', '🍌', '🍋']);
+
+/////////////////////////////////////////
+
+// function printContactsInfo({ names, phones }) {
+//     const nameList = names.split(',');
+//     const phoneList = phones.split(',');
+//     // for (let i = 0; i < nameList.length; i += 1) {
+//     //   console.log(`${nameList[i]}: ${phoneList[i]}`);
+//     // }
+//     nameList.forEach((name,i) => console.log(`${nameList[i]}: ${phoneList[i]}`))
+// }
+  
+//   printContactsInfo({
+//     names: 'Jacob,William,Solomon,Artemis',
+//     phones: '89001234567,89001112233,890055566377,890055566300',
+//   });
+
+///////////////////////////////
+
+// function calсulateAverage(...args) {
+//     let total = 0;
+//     // for (let i = 0; i < args.length; i++) {
+//     //   total += args[i];
+//     // }
+//     // return total / args.length;
+
+//     args.forEach((item) => (total += item));
+
+//     return total / args.length;
+//   }
+  
+//   console.log(calсulateAverage(1, 2, 3, 4)); // 2.5
+//   console.log(calсulateAverage(14, 8, 2)); // 8
+//   console.log(calсulateAverage(27, 43, 2, 8, 36)); // 23.2
+
+///////////////////////////////////////
+// function abbrevName(name){
+//   const letter = name[0];
+//     for (i>0; i<name.length; i+=1){
+//       if(name[i]=name.toUpperCase()){
+//       return letter.push(name[i])
+//     }
+//     }
+// }
+function abbrevName(name){
+  const answer = [name[0]]
+  for(let i = 0; i < name.length; i++) {
+      if(name[i] === " ") {
+          answer.push(name[i + 1]);
+      }
+  }
+  return answer.map(el => el.toUpperCase()).join(".")
 }
-
-
-function removeCourse(name){
-    const index = courses.indexOf(name)
-
-    if (index != -1){
-        courses.splice(index, 1);
-    } else{
-        console.log('Courses not found')
-    }
-}
-
-
-function updateCourse(oldName, newName){
-    const index = courses.indexOf(oldName)
-
-    if (index != -1){
-        courses.splice(index, 1, newName);
-    } else{
-        console.log('Courses not found')
-    }
-}
-addCourse('Express');
-console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'React', 'PostgreSQL', 'Express']
-addCourse('CSS'); // 'У вас уже есть такой курс'
-
-removeCourse('React');
-// console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'Express']
-removeCourse('Vue'); // 'Курс с таким имененем не найден'
-
-updateCourse('Express', 'NestJS');
-console.log(courses); // ['HTML', 'CSS', 'JavaScript', 'PostgreSQL', 'NestJS']
+console.log(abbrevName(Sam, Harris))
